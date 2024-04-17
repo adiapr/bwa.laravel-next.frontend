e4315efb93e9207aba68fb54a1fca836343ea553
adiapr <da801100@gmail.com>
2024-04-17 20:41:17 +0700

    Listing Showcase

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 export { default } from "next-auth/middleware"
 
+// untuk menentukan routing mana saja yang dapat diakses 
 export const config = { matcher: [
     "/listing/:id/checkout",
     "/booking-success/:path*",
-    "/dashboard"
+    "/dashboard/:path*"
 ] }
